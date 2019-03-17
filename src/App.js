34948fdf7659e50ca9fd6eae5d91025495ba5bd0
wxh1107 <1486838332@qq.com>
2019-03-17 22:40:59 +0800

    创建

--- src/App.js
+++ src/App.js
@@ -1,28 +1,129 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Table } from 'antd';
+import { Resizable } from 'react-resizable';
+import ReactDragListView from "react-drag-listview"
+import './App.css'
+import 'antd/dist/antd.css'
 
-class App extends Component {
+
+const ResizeableTitle = (props) => {
+  console.log(props)
+  const { onResize, width, ...restProps } = props;
+
+  if (!width) return <th {...restProps} />
+
+  return (
+    <Resizable width={width} height={0} onResize={onResize}>
+      <th {...restProps} />
+    </Resizable>
+  )
+}
+
+class DragTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        key: 0,
+        date: '2018-02-11',
+        amount: 120,
+        type: 'income',
+        note: 'transfer',
+      }, {
+        key: 1,
+        date: '2018-03-11',
+        amount: 243,
+        type: 'income',
+        note: 'transfer',
+      }, {
+        key: 2,
+        date: '2018-04-11',
+        amount: 98,
+        type: 'income',
+        note: 'transfer',
+      }],
+      columns: [{
+        title: <div className="drag">Date</div>,
+        dataIndex: 'date',
+        width: 200,
+      }, {
+        title: <div className="drag">Amount</div>,
+        dataIndex: 'amount',
+        width: 100,
+      }, {
+        title: <div className="drag">Type</div>,
+        dataIndex: 'type',
+        width: 100,
+      }, {
+        title: <div className="drag">Note</div>,
+        dataIndex: 'note',
+        width: 100,
+      }, {
+        title: <div className="drag">Action</div>,
+        key: 'action',
+        render: () => (
+          <a href="javascript:;">Delete</a>
+        ),
+      }],
+    };
+
+    const that = this;
+    this.dragProps = {
+      onDragEnd(fromIndex, toIndex) {
+        const columns = that.state.columns;
+        const item = columns.splice(fromIndex, 1)[0];
+        columns.splice(toIndex, 0, item);
+        that.setState({
+          columns
+        });
+      },
+      nodeSelector: "th",
+      handleSelector: '.drag',
+    };
+  }
+
+  handleResize = index => (e, { size }) => {
+    this.setState(({ columns }) => {
+      const nextColumns = [...columns];
+      nextColumns[index] = {
+        ...nextColumns[index],
+        width: size.width,
+      };
+      return { columns: nextColumns };
+    });
+  };
+
+  components = {
+    header: {
+      cell: ResizeableTitle,
+    },
+  };
+
   render() {
+    const { data }=this.state
+    const columns = this.state.columns.map((col, index) => ({
+      ...col,
+      onHeaderCell: column => ({
+        width: column.width,
+        onResize: this.handleResize(index),
+      }),
+    }));
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ReactDragListView.DragColumn {...this.dragProps}>
+            <Table
+              columns={columns}
+              pagination={false}
+              components={this.components}
+              dataSource={data}
+              bordered
+            />
+          </ReactDragListView.DragColumn>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default DragTable;
